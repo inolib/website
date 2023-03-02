d161e3130778800a9f434061de9af34c4a09412d
matthieu <georgematthieu1@gmail.com>
2023-03-02 17:24:09 +0100

    wip css banner

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,21 +3,28 @@ import { type DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
-    <main>
-      <section className="mx-[10vw] bg-[url('/image/Home-image.png')]">
-        <span className="flex justify-center">
-          {/* <img className="w-[50vw] h-[70vh]" src="\images\decouvriraccess.png" alt="" />
-          <img className="w-[30vw]" src="\images\backgroundcartehome.png" alt="" />
-          <div className="bg-white">Découvrir l'accessibilité</div> */}
-        </span>
-        <span className="flex justify-start mt-10">
-          <h2 className="text-[#0B3168] font-normal text-4xl">Nos services</h2>
-        </span>
+    <main class="flex justify-center flex-col">
+      <section className="mx-[10vw] w-[85vw] h-[75vh] flex items-center justify-end bg-[length:80vw_80vh] bg-no-repeat bg-[url('/images/Home-image.png')]">
+        <div class="bg-white w-[38vw] h-[40vh] pr-10 mr-[12vw] mb-[16vh]">
+          <span className=" max-w-[5vw]">
+            <h2 className="text-[#0B3168] text-3xl ml-10 mt-10 font-semibold">Découvrir l'accessibilité</h2>
+            <p className="text-[#0B3168] ml-10 mt-10 font-extralight flex">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, magnam accusamus dolores soluta deserunt
+              nesciunt, molestiae, architecto velit ex doloremque voluptatum eos itaque veritatis quas odit.
+              <a href="/accessibilite">
+                <img
+                  className="w-[5vw] h-[5vh] ml-4 mt-4 px-4"
+                  src="/images/icon-small-arrow.svg"
+                  alt="lien vers page accessibilité"
+                />
+              </a>
+            </p>
+          </span>
+        </div>
       </section>
-      <article className="bg-[url('/image/bg-cards.png')]"></article>
-      <article></article>
-      <article></article>
-      <article></article>
+      <span className="flex justify-start mt-10">
+        <h2 className="text-[#0B3168] font-normal text-4xl">Nos services</h2>
+      </span>
     </main>
   );
 });
