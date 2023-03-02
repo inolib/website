b9fa734b4a5a733293ef18fc0fce48b0a4d0c686
Quentin Ferrari <ferrari.quentin@gmail.com>
2023-03-02 11:37:11 +0100

    feat: :art: continue css on footer, mobile version finished, started desktop version

--- src/components/footer/Contentinfo.tsx
+++ src/components/footer/Contentinfo.tsx
@@ -3,40 +3,42 @@ import { component$ } from "@builder.io/qwik";
 export const Contentinfo = component$(() => {
   return (
     <footer>
-      <section className="bg-gradient-to-b from-[#5A0C69] to-[#0F3069] text-white p-4">
-        <section>
+      <section className="bg-gradient-to-b from-[#5A0C69] to-[#0F3069] text-white p-4 md:flex">
+        <section className="md:flex md:flex-col md:items-center md:w-1/2">
           <div className="flex mb-4">
             <img alt="" src="/images/logo-white.svg" className="h-10 w-auto" />
-            <h3>INOLIB</h3>
+            <h3 className="flex items-center pl-2">INOLIB</h3>
           </div>
-          <p className="mb-4">Des questions?</p>
-          <p className="mb-4">
+          <p className="border-t-2 border-[#9A35AD] mb-8 pt-4">Des questions?</p>
+          <p className="mb-8">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, magnam accusamus dolores soluta deserunt
             nesciunt, molestiae, architecto velit ex doloremque voluptatum eos itaque veritatis quas odit. Eveniet saepe
             dolore quae!
           </p>
           <a
-            className="items-center bg-white flex cursor-pointer drop-shadow-md h-10 justify-center mb-4 rounded-md text-[#0b3168] w-100%"
+            className="items-center bg-white flex cursor-pointer drop-shadow-md h-10 justify-center mb-8 rounded-md text-[#0b3168] w-100% lg:w-36"
             href="/contact"
           >
             Nous contacter
           </a>
         </section>
-        <section className="flex justify-between">
-          <a href="#">
-            <img alt="facebook INOLIB" src="\images\logo-facebook-violet.svg"></img>
-          </a>
-          <a href="#">
-            <img alt="twitter INOLIB" src="\images\logo-twiter.svg"></img>
-          </a>
-          <a href="#">
-            <img alt="LinkedIn INOLIB" src="\images\logo-linkedin.svg"></img>
-          </a>
-          <a href="#">
-            <img alt="instagram INOLIB" src="\images\logo-instagram.svg"></img>
-          </a>
-        </section>
-        <p>© 2023 Inolib</p>
+        <div className="md:flex md:flex-col">
+          <section className="border-y-2 border-[#9A35AD] flex justify-between mb-8 py-4 md:w1/2">
+            <a href="#">
+              <img alt="facebook INOLIB" src="\images\logo-facebook-violet.svg"></img>
+            </a>
+            <a href="#">
+              <img alt="twitter INOLIB" src="\images\logo-twiter.svg"></img>
+            </a>
+            <a href="#">
+              <img alt="LinkedIn INOLIB" src="\images\logo-linkedin.svg"></img>
+            </a>
+            <a href="#">
+              <img alt="instagram INOLIB" src="\images\logo-instagram.svg"></img>
+            </a>
+          </section>
+          <p>© 2023 Inolib</p>
+        </div>
       </section>
     </footer>
   );
