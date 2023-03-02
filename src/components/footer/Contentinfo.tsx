d540808b846409d1cef1b291397518bb13bb05f6
Quentin Ferrari <ferrari.quentin@gmail.com>
2023-03-02 15:42:25 +0100

    feat: working on responsive footer
    footer is now responsive mobile en desktop version

--- src/components/footer/Contentinfo.tsx
+++ src/components/footer/Contentinfo.tsx
@@ -3,42 +3,53 @@ import { component$ } from "@builder.io/qwik";
 export const Contentinfo = component$(() => {
   return (
     <footer>
-      <section className="bg-gradient-to-b from-[#5A0C69] to-[#0F3069] text-white p-4 md:flex">
-        <section className="md:flex md:flex-col md:items-center md:w-1/2">
-          <div className="flex mb-4">
-            <img alt="" src="/images/logo-white.svg" className="h-10 w-auto" />
-            <h3 className="flex items-center pl-2">INOLIB</h3>
+      <section class="bg-gradient-to-b from-[#5A0C69] to-[#0F3069] text-white py-10 md:flex">
+        <section class="md:flex md:flex-col md:items-end md:px-8 md:w-1/2">
+          <div class="md:w-2/3">
+            <div class="flex mb-3">
+              <img alt="" src="/images/logo-white.svg" className="h-10 w-auto" />
+              <h3 class="flex items-center font-bold pl-2 text-2xl">INOLIB</h3>
+            </div>
+            <p class="border-t-2 border-[#9A35AD] font-bold mb-6 pt-3 md:border-t-0">Des questions?</p>
+            <h3 class="font-bold mb-3 text-2xl">Contactez-nous!</h3>
+            <p class="mb-5">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, magnam accusamus dolores soluta deserunt
+              nesciunt, molestiae, architecto velit ex doloremque voluptatum eos itaque veritatis quas.
+            </p>
+            <a
+              class="items-center bg-white flex cursor-pointer drop-shadow-md h-10 justify-center mb-8 rounded-md text-[#0b3168] w-100% lg:w-36"
+              href="/contact"
+            >
+              Nous contacter
+            </a>
           </div>
-          <p className="border-t-2 border-[#9A35AD] mb-8 pt-4">Des questions?</p>
-          <p className="mb-8">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, magnam accusamus dolores soluta deserunt
-            nesciunt, molestiae, architecto velit ex doloremque voluptatum eos itaque veritatis quas odit. Eveniet saepe
-            dolore quae!
-          </p>
-          <a
-            className="items-center bg-white flex cursor-pointer drop-shadow-md h-10 justify-center mb-8 rounded-md text-[#0b3168] w-100% lg:w-36"
-            href="/contact"
-          >
-            Nous contacter
-          </a>
         </section>
-        <div className="md:flex md:flex-col">
-          <section className="border-y-2 border-[#9A35AD] flex justify-between mb-8 py-4 md:w1/2">
-            <a href="#">
-              <img alt="facebook INOLIB" src="\images\logo-facebook-violet.svg"></img>
-            </a>
-            <a href="#">
-              <img alt="twitter INOLIB" src="\images\logo-twiter.svg"></img>
-            </a>
-            <a href="#">
-              <img alt="LinkedIn INOLIB" src="\images\logo-linkedin.svg"></img>
-            </a>
-            <a href="#">
-              <img alt="instagram INOLIB" src="\images\logo-instagram.svg"></img>
-            </a>
-          </section>
-          <p>© 2023 Inolib</p>
-        </div>
+
+        <section class="md:border-l-2 md:grid md:grid-rows-1 md:grid-cols-1 md:pl-10 md:w-1/4">
+          <ul class="border-y-2 border-[#9A35AD] flex justify-between py-8 md:border-y-0 md:items-center md:columns-1 md:py-0 md:row-start-1 ">
+            <li>
+              <a href="#">
+                <img alt="facebook INOLIB" src="\images\logo-facebook-violet.svg"></img>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img alt="twitter INOLIB" src="\images\logo-twiter.svg"></img>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img alt="LinkedIn INOLIB" src="\images\logo-linkedin.svg"></img>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img alt="instagram INOLIB" src="\images\logo-instagram.svg"></img>
+              </a>
+            </li>
+          </ul>
+          <p class="mt-8 md:items-end md:columns-1 md:row-start-">© 2023 Inolib</p>
+        </section>
       </section>
     </footer>
   );
