ef540fc96278b9c853558b52185096f580eb5ab3
Matthieu Meignan <matthieu.meignan@inolib.com>
2023-03-02 03:08:50 +0100

    feat: add <title> in <head>

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,9 +1,14 @@
 import { component$ } from "@builder.io/qwik";
+import { useDocumentHead } from "@builder.io/qwik-city";
 
 export const Head = component$(() => {
+  const head = useDocumentHead();
+
   return (
     <head>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+
+      <title>{head.title}</title>
     </head>
   );
 });
