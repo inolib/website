37b7704cfa162da5c821a28ca4f477c902c8c2c5
matthieu <georgematthieu1@gmail.com>
2023-03-02 14:27:34 +0100

    wip css banner

--- src/components/header/Banner.tsx
+++ src/components/header/Banner.tsx
@@ -11,7 +11,7 @@ export const Banner = component$(() => {
           <span className="flex flex-col border-l font-bold text-white max-w-[5vw] h-[50vh] text-[300%] pl-10">
             HUMAN NOBLE TECHNOLOGY
             <a
-              className="bg-white border-[#0B3168] border-[1px] hover:border-[2px] mt-10 pl-[3.2vw] py-[2vh] text-[#0B3168] text-xl rounded-md ml-[10vw] w-[12vw] font-normal"
+              className="bg-white border-[#0B3168] border-[1px] hover:border-[3px] mt-10 pl-[3vw] py-[2vh] text-[#0B3168] text-xl rounded-md ml-[10vw] w-[12vw] font-normal"
               href="/presentation"
             >
               Découvrir
@@ -19,9 +19,9 @@ export const Banner = component$(() => {
           </span>
         </h1>
         <section class="flex justify-around mt-[21vh]">
-          <img class="ml-[20vw]" src="/images/logo-hup-violet.svg" alt="logo hup" />
+          <img class="ml-[25vw]" src="/images/logo-hup-violet.svg" alt="logo hup" />
           <img src="/images/logo-fidev-violet.svg" alt="logo hup" />
-          <img class="mr-[20vw]" src="/images/logo-apside-violet.svg" alt="logo hup" />
+          <img class="mr-[25vw]" src="/images/logo-apside-violet.svg" alt="logo hup" />
         </section>
       </section>
     </header>
