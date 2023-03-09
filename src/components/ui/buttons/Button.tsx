8ee10f11b3f0452cff0ee806292f30ddc2df0cde
Matthieu MEIGNAN <matthieu.meignan@inolib.com>
2023-03-09 16:23:10 +0100

    fix: Tailwind colors

--- src/components/ui/buttons/Button.tsx
+++ src/components/ui/buttons/Button.tsx
@@ -8,12 +8,12 @@ type ButtonProps = {
 };
 
 export const Button = component$((props: ButtonProps) => {
-  const primaryColor = props.color === "white" ? "#ffffff" : "#0b3168";
-  const secondaryColor = props.color === "white" ? "#0b3168" : "#ffffff";
+  const primaryColor = props.color === "white" ? "white" : "[#0B3168]";
+  const secondaryColor = props.color === "white" ? "[#0B3168]" : "white";
   const styles = props.styles !== undefined ? props.styles : "";
 
   return (
-    <a class={`bg-[${primaryColor}] px-8 py-4 text-[${secondaryColor}] text-xl rounded-md ${styles}`} href={props.href}>
+    <a class={`bg-${primaryColor} px-8 py-4 text-${secondaryColor} text-xl rounded-md ${styles}`} href={props.href}>
       {props.label}
     </a>
   );
