d091985cda65f03a945251c2b0efc5b16ae6d67c
Quentin Ferrari <ferrari.quentin@gmail.com>
2023-03-10 13:30:59 +0100

    deuxième article page présentation refactorisé

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -8,10 +8,15 @@ type ArticleProps = {
   imageSrc: string;
   imageQuote?: string;
   styles?: string;
+  stylesArticleGaucheFlex?: string;
+  stylesArticleGauchePadding?: string;
 };
 
 export const Article = component$((props: ArticleProps) => {
   const styles = props.styles !== undefined ? props.styles : "";
+  const stylesArticleGaucheFlex = props.stylesArticleGaucheFlex !== undefined ? props.stylesArticleGaucheFlex : "";
+  const stylesArticleGauchePadding =
+    props.stylesArticleGauchePadding !== undefined ? props.stylesArticleGauchePadding : "";
 
   return (
     <article class="md:px-28 md:pt-4">
@@ -19,9 +24,11 @@ export const Article = component$((props: ArticleProps) => {
         <h2 class={`text-2xl font-semibold `}>{props.title}</h2>
         <p>{props.titleQuote}</p>
       </span>
-      <div class="md:flex">
-        <div class={`md:px-4 md:pr-10 md:pl-0 md:w-[46vw]`}>{props.content}</div>
-        <p>{props.contentQuote}</p>
+      <div class={`flex ${stylesArticleGaucheFlex}`}>
+        <span class={`${stylesArticleGauchePadding}`}>
+          <div class={`md:px-4 md:pr-10 md:pl-0 md:w-[46vw]`}>{props.content}</div>
+          <p>{props.contentQuote}</p>
+        </span>
         <div class="md:w-1/3">
           <img src={props.imageSrc} alt="" />
           <strong>
--- src/routes/presentation/index.tsx
+++ src/routes/presentation/index.tsx
@@ -34,47 +34,39 @@ export default component$(() => {
           imageQuote="“ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ”"
         ></Article>
 
-        <article class=" md:px-28">
-          <h2 class="text-2xl font-bold my-6">Ce que nous sommes au quotidien</h2>
-          <div class="md:flex">
-            <div class="max-w-lg px-4 md:pr-10">
-              <img class="mb-6" src="..\images\presentation-article-2.png" />
-              <blockquote class="px-4">
-                <strong>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet, culpa alias? Officia autem
-                  doloribus nemo cul.
-                </strong>
-              </blockquote>
-            </div>
-            <div class="my-10 md:mb-10 md:mt-0  md:w-[46vw]">
-              <p class="text-justify md:text-left">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium vel, enim, dolore dolorem a
-                mollitia adipisci ipsa pariatur ipsum officiis eligendi expedita. Eaque cumque suscipit quidem
-                exercitationem. Ipsa, eveniet eaque. At modi porro, perspiciatis enim illum laborum nostrum nesciunt
-                voluptatum doloremque vitae, consequuntur eaque aliquam deserunt pariatur sequi possimus, tempora quidem
-                quas repellendus dolore. Illum accusantium sed dolores aperiam. Deserunt. Aliquam dolor facere officiis
-                suscipit! Nobis repellat porro molestiae temporibus, totam sed cupiditate excepturi eligendi animi esse?
-                Voluptas, at molestiae, quis magnam temporibus ut in minus voluptate soluta eaque enim. Odit, eligendi
-                similique. Odit, saepe at! Nisi nemo exercitationem impedit obcaecati assumenda accusantium. A, omnis
-                laboriosam unde laudantium quia corrupti. Animi, optio? Perspiciatis incidunt in fugiat laudantium
-                expedita, repellendus cum. Eaque sint illo dignissimos quod cum ex, possimus, sed minus omnis voluptate
-                alias aspernatur maxime quasi quibusdam hic officiis inventore ea placeat consectetur illum totam quia
-                tenetur fugit libero? Voluptate? Optio ex id maiores perferendis molestias saepe debitis atque aut
-                quaerat commodi hic, nam illo et labore ea quasi facilis maxime incidunt fugit necessitatibus
-                laboriosam! Temporibus consequuntur sunt enim facilis. Tempore alias, earum officiis ut est labore
-                distinctio fugit voluptatibus quis minus repudiandae saepe dolorem aperiam quibusdam odio, veniam rerum
-                officia eius libero obcaecati dolor quisquam, omnis dolorum nostrum. Neque? Minima tempore laudantium
-                neque accusamus cum, aspernatur, culpa ipsum inventore qui possimus ullam ratione sint aliquam iusto
-                eveniet ducimus dicta necessitatibus nostrum corrupti itaque architecto hic dolor quas. Quibusdam, quam?
-                Culpa a incidunt natus! Sint fugit dolores debitis. Aliquam totam neque ullam dolore. Doloremque
-                sapiente maiores natus nulla quisquam ducimus aperiam porro rerum sunt? Facilis voluptatum eum modi
-                neque dignissimos? Cupiditate iusto voluptates enim ipsa cum qui, omnis beatae fugiat aperiam dolore
-                provident porro quasi, ab molestiae nulla similique illum blanditiis consectetur harum, non molestias
-                laborum nihil voluptate nemo. Exercitationem.
-              </p>
-            </div>
-          </div>
-        </article>
+        <Article
+          title="Ce que nous sommes au quotidien"
+          styles="py-6"
+          stylesArticleGaucheFlex="flex-row-reverse justify-end"
+          stylesArticleGauchePadding="pl-10"
+          content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium vel, enim, dolore dolorem a
+          mollitia adipisci ipsa pariatur ipsum officiis eligendi expedita. Eaque cumque suscipit quidem
+          exercitationem. Ipsa, eveniet eaque. At modi porro, perspiciatis enim illum laborum nostrum nesciunt
+          voluptatum doloremque vitae, consequuntur eaque aliquam deserunt pariatur sequi possimus, tempora quidem
+          quas repellendus dolore. Illum accusantium sed dolores aperiam. Deserunt. Aliquam dolor facere officiis
+          suscipit! Nobis repellat porro molestiae temporibus, totam sed cupiditate excepturi eligendi animi esse?
+          Voluptas, at molestiae, quis magnam temporibus ut in minus voluptate soluta eaque enim. Odit, eligendi
+          similique. Odit, saepe at! Nisi nemo exercitationem impedit obcaecati assumenda accusantium. A, omnis
+          laboriosam unde laudantium quia corrupti. Animi, optio? Perspiciatis incidunt in fugiat laudantium
+          expedita, repellendus cum. Eaque sint illo dignissimos quod cum ex, possimus, sed minus omnis voluptate
+          alias aspernatur maxime quasi quibusdam hic officiis inventore ea placeat consectetur illum totam quia
+          tenetur fugit libero? Voluptate? Optio ex id maiores perferendis molestias saepe debitis atque aut
+          quaerat commodi hic, nam illo et labore ea quasi facilis maxime incidunt fugit necessitatibus
+          laboriosam! Temporibus consequuntur sunt enim facilis. Tempore alias, earum officiis ut est labore
+          distinctio fugit voluptatibus quis minus repudiandae saepe dolorem aperiam quibusdam odio, veniam rerum
+          officia eius libero obcaecati dolor quisquam, omnis dolorum nostrum. Neque? Minima tempore laudantium
+          neque accusamus cum, aspernatur, culpa ipsum inventore qui possimus ullam ratione sint aliquam iusto
+          eveniet ducimus dicta necessitatibus nostrum corrupti itaque architecto hic dolor quas. Quibusdam, quam?
+          Culpa a incidunt natus! Sint fugit dolores debitis. Aliquam totam neque ullam dolore. Doloremque
+          sapiente maiores natus nulla quisquam ducimus aperiam porro rerum sunt? Facilis voluptatum eum modi
+          neque dignissimos? Cupiditate iusto voluptates enim ipsa cum qui, omnis beatae fugiat aperiam dolore
+          provident porro quasi, ab molestiae nulla similique illum blanditiis consectetur harum, non molestias
+          laborum nihil voluptate nemo. Exercitationem."
+          imageSrc="\images\presentation-article-2.png"
+          imageQuote="“Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet, culpa alias? Officia autem
+          doloribus nemo cul.“"
+        ></Article>
+
         <section class="bg-gradient-to-b from-[#5A0C69] to-[#0F3069] text-white flex px-4 py-8 md:px-36">
           <div class="flex flex-col justify-center md:pr-16">
             <strong>
