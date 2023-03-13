5e72c15b48b6e8d7777a0a2c8775b3b8d57156f9
matthieu <georgematthieu1@gmail.com>
2023-03-13 14:46:18 +0100

    refactoring

--- src/components/cards/CardsRowDisplay.tsx
+++ src/components/cards/CardsRowDisplay.tsx
@@ -11,21 +11,21 @@ export const CardsRowDisplay = component$(() => {
         <h2 class="text-[#0B3168] font-semibold text-4xl">Nos services</h2>
       </span>
       <section class="flex  justify-around my-[4rem] mx-[10rem]">
-        <ServicesHomeCard label="Conseil et Audit" image="/images/logo-audit.png" background="purple">
+        <ServicesHomeCard label="Conseil et Audit" image="/images/logo-audit.png">
           <p q:slot="content" class="text-[#0B3168]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aperiam amet fuga, obcaecati ratione optio,
             modi nostrum magnam tenetur assumenda labore aliquam accusamus nemo hic corporis consequatur autem!
             Molestias, facilis?
           </p>
         </ServicesHomeCard>
-        <ServicesHomeCard label="Formation" image="/images/logo-formation.png" background="purple">
+        <ServicesHomeCard label="Formation" image="/images/logo-formation.png">
           <p q:slot="content" class="text-[#0B3168]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aperiam amet fuga, obcaecati ratione optio,
             modi nostrum magnam tenetur assumenda labore aliquam accusamus nemo hic corporis consequatur autem!
             Molestias, facilis?
           </p>
         </ServicesHomeCard>
-        <ServicesHomeCard label="Développement" image="/images/logo-dev.png" background="purple">
+        <ServicesHomeCard label="Développement" image="/images/logo-dev.png">
           <p q:slot="content" class="text-[#0B3168]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aperiam amet fuga, obcaecati ratione optio,
             modi nostrum magnam tenetur assumenda labore aliquam accusamus nemo hic corporis consequatur autem!
--- src/components/cards/ServicesCard.tsx
+++ src/components/cards/ServicesCard.tsx
@@ -7,23 +7,19 @@ type CardProps = {
   title?: string;
   label?: string;
   styles?: string;
-  background?: "purple" | "white";
 };
 
 export const ServicesCard = component$((props: CardProps) => {
   const styles = props.styles !== undefined ? props.styles : "";
-  const background = props.background !== undefined ? props.background : "";
-  const primaryBackground = props.background === "purple" ? "[url('/images/bg-cards.png')]" : "white";
-  const secondaryBackground = props.background === "purple" ? "white" : "[url(/images/bg-cards.png)]";
 
   return (
     <>
       <article aria-label="articles" class={`flex flex-col`}>
         <span
-          class={`flex flex-col justify-center items-center bg-no-repeat bg-${primaryBackground} bg-[length:110rem_40rem] h-[18rem] w-[22.5rem]`}
+          class={`flex flex-col justify-center items-center bg-no-repeat bg-[url(/images/bg-cards.png)] bg-[length:110rem_40rem] h-[18rem] w-[22.5rem]`}
         >
           <img class="-mt-[5rem]" src={props.image} alt="" />
-          <h2 class=" font-normal text-center text-2xl -mt-[5rem] text-white  ">{props.title}</h2>
+          <h2 class=" font-normal text-center text-2xl -mt-[5rem] text-white">{props.title}</h2>
         </span>
         <span class={`flex flex-col items-top justify-start shadow-xl px-4 py-5 h-[30rem] w-[22.5rem]`}>
           <Slot name="services" />
