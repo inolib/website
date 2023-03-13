f49f908197fddfde51a82fd3b35d84910c92f97e
matthieu <georgematthieu1@gmail.com>
2023-03-13 16:28:44 +0100

    refactoring

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, Slot } from "@builder.io/qwik";
 
 type ArticleProps = {
   title?: string;
@@ -9,21 +9,24 @@ type ArticleProps = {
   imageQuote?: string;
   articleTestimonyStyles?: string;
   imgSeparator?: string;
+  styles?: string;
   stylesArticleGaucheFlex?: string;
   stylesArticlePadding?: string;
   stylesImgArticle?: string;
   typoImgQuote?: "bold | normal";
   testimony?: string;
+  imageText?: string;
 };
 
 export const Article = component$((props: ArticleProps) => {
   const articleTestimonyStyles = props.articleTestimonyStyles !== undefined ? props.articleTestimonyStyles : "";
+  const styles = props.styles !== undefined ? props.styles : "";
   const stylesArticleGaucheFlex = props.stylesArticleGaucheFlex !== undefined ? props.stylesArticleGaucheFlex : "";
   const stylesImgArticle = props.stylesImgArticle !== undefined ? props.stylesImgArticle : "";
   const imgQuoteSize = props.typoImgQuote === "bold" ? "font-bold text-l" : "";
 
   return (
-    <article class="md:px-28 md:py-8">
+    <article class="md:mx-[8rem] md:py-8">
       <h2 class={`text-2xl font-semibold `}>{props.title}</h2>
 
       <div class={`flex pt-6 ${stylesArticleGaucheFlex}`}>
@@ -33,7 +36,10 @@ export const Article = component$((props: ArticleProps) => {
             <img class="px-4" src={props.imgSeparator} alt="" />
             <p class="text-l">{props.titleQuote}</p>
           </span>
-          <div class={`md:px-4 md:pr-10 md:pl-0 md:w-[46vw]`}>{props.content}</div>
+          <div class={`md:px-4 md:pr-10 md:pl-0 md:w-[46vw]`}>
+            {props.content}
+            <Slot name="check-box" />
+          </div>
 
           <strong class="text-xl text-justify flex justify-center mt-16">
             <p class="w-[25rem] text-center">{props.contentQuote}</p>
@@ -42,6 +48,7 @@ export const Article = component$((props: ArticleProps) => {
         <div class={`flex flex-col ${stylesImgArticle}`}>
           <img src={props.imageSrc} alt="" />
           <blockquote class={`mt-6 text-center max-w-content  ${imgQuoteSize}`}>{props.imageQuote}</blockquote>
+          <p class={`mt-6 text-center max-w-content ${styles} ${imgQuoteSize}`}>{props.imageText}</p>
         </div>
       </div>
     </article>
