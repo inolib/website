1b7b8272f15f5d659049babf9f5182c9a2f5114a
matthieu <georgematthieu1@gmail.com>
2023-03-17 11:04:03 +0100

    responsive

--- src/components/cards/CardsRowDisplay.tsx
+++ src/components/cards/CardsRowDisplay.tsx
@@ -2,15 +2,16 @@ import { component$ } from "@builder.io/qwik";
 
 import { ServicesHomeCard } from "~/components/cards/ServicesHomeCard";
 import { ServicesHomeCardBlank } from "~/components/cards/ServicesHomeCardBlank";
+import { Button } from "../ui/buttons/Button";
 
 export const CardsRowDisplay = component$(() => {
   return (
     <>
-      <span class="flex justify-start mt-10 ml-[10rem]">
-        <h2 class="text-[#0B3168] font-semibold text-4xl">Nos services</h2>
+      <span class="flex justify-start mt-10 ml-5 md:ml-[10rem]">
+        <h2 class="text-[#0B3168] font-semibold text-2xl md:text-4xl">Nos services</h2>
       </span>
-      <section class="flex  justify-around my-[4rem] mx-[10rem]">
-        <ServicesHomeCard label="Conseil et Audit" image="/images/logo-audit.png">
+      <section class="flex md:flex-row flex-col justify-around md:my-[4rem] md:mx-[10rem]">
+        <ServicesHomeCard label="Conseil/et/Audit" image="/images/logo-audit.png">
           <p q:slot="content" class="text-[#0B3168]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aperiam amet fuga, obcaecati ratione optio,
             modi nostrum magnam tenetur assumenda labore aliquam accusamus nemo hic corporis consequatur autem!
@@ -31,10 +32,16 @@ export const CardsRowDisplay = component$(() => {
             Molestias, facilis?
           </p>
         </ServicesHomeCard>
+
         <ServicesHomeCardBlank
-          styles="[10rem]"
+          styles="my-[10rem]"
           content="Découvrez tout nos services en détails ! Lorem ipsum dolor sit amet consectetur adipisicing elit."
-        />
+        >
+          <Button q:slot="button" color="blue" href="/services" label="En savoir plus" styles="hidden md:block" />
+        </ServicesHomeCardBlank>
+        <span class={`flex justify-center mt-[2rem]`}>
+          <Button color="blue" href="/services" label="Tous nos services" styles="md:hidden flex justify-center" />
+        </span>
       </section>
     </>
   );
--- src/components/cards/SplitedImage.tsx
+++ src/components/cards/SplitedImage.tsx
@@ -8,19 +8,19 @@ type ImageProps = {
 export const SplitedImage = component$((props: ImageProps) => {
   return (
     <>
-      <section class="relative mx-[10rem] flex">
+      <section class="relative md:mx-[10rem] flex">
         <img src={props.src} alt="" />
         <span class="absolute flex">
           <DiscoverAcessCard
-            styles="ml-[35rem] mt-[5rem]"
-            alt="lien vers page accessibilité"
+            styles="md:ml-[35rem] md:mt-[5rem]"
+            alt="aller vers page accessibilité"
             href="/accessibilite"
             title="Découvrir l'accessibilité"
             content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, magnam accusamus dolores soluta deserunt
           nesciunt, molestiae, architecto velit ex doloremque voluptatum eos itaque veritatis quas odit."
           />
         </span>
-        <img class=" w-[40rem]" src="images/bg-cards.png" alt="" />
+        <img class=" md:w-[40rem]" src="images/bg-cards.png" alt="" />
       </section>
       ;
     </>
