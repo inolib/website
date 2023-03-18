2f8445b9e71b7892492e402c89ca4757164a5f68
Matthieu MEIGNAN <matthieu.meignan@inolib.com>
2023-03-18 07:16:06 +0100

    chore: update Prettier config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,3 +1,17 @@
+/** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
 module.exports = {
+  plugins: [
+    require("prettier-plugin-packagejson"),
+    require("prettier-plugin-prisma"),
+    require.resolve("@ianvs/prettier-plugin-sort-imports"),
+    require.resolve("prettier-plugin-jsdoc"),
+    require("prettier-plugin-tailwindcss"),
+  ],
   printWidth: 120,
+  importOrder: ["^\\~/(.*)$"],
+  importOrderCaseInsensitive: true,
+  importOrderCombineTypeAndValueImports: true,
+  importOrderMergeDuplicateImports: true,
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 };
