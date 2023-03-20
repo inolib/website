0611177c66eef6a3e508dbb595603ede25cb6837
Quentin Ferrari <ferrari.quentin@gmail.com>
2023-03-20 11:39:45 +0100

    page contact responsive

--- src/components/ui/ContactForm.tsx
+++ src/components/ui/ContactForm.tsx
@@ -2,9 +2,9 @@ import { component$ } from "@builder.io/qwik";
 
 export const ContactForm = component$(() => {
   return (
-    <form class="flex md:grid md:grid-cols-4 md:grid-rows-5 md:px-10 py-14 md:w-2/3">
+    <form class="grid grid-cols-4 grid-rows-7 md:grid-rows-5 md:px-10 py-14 md:w-2/3">
       <select
-        class="border-solid border-[1px] border-[#0B3168] flex h-12 rounded-md md:col-span-1 md:col-start-1 md:col-end-3 md:row-start-1"
+        class="border-solid border-[1px] border-[#0B3168] flex h-12 rounded-md col-span-5 md:col-span-2 col-start-1 col-end-5 md:col-end-3 row-start-1"
         name="Type de la demande"
         aria-label="type de votre demande"
         required
@@ -17,16 +17,16 @@ export const ContactForm = component$(() => {
         <option>Développement</option>
       </select>
 
-      <label class="md:col-span-1 md:row-start-2 md:col-start-1  md:col-end-3 md:flex md:flex-col md:pr-5">
+      <label class="md:col-span-2 col-span-4 row-start-2 col-start-1 md:col-end-3 col-end-5 flex flex-col mb-3 md:mb-0 md:pr-5">
         Nom
         <input
-          class="border-solid border-[1px] border-[#0B3168] pl-2 md:h-12 rounded-md"
+          class="border-solid border-[1px] border-[#0B3168] pl-2 md:mb-0 md:h-12 rounded-md"
           required
           type="text"
           aria-label="entrez votre nom"
         />
       </label>
-      <label class="md:col-span-2 md:row-start-2 md:col-start-3  md:col-end-5 md:flex md:flex-col md:pl-5">
+      <label class="md:col-span-2 col-span-4 row-start-3 col-start-1 col-end-5 flex flex-col mb-3 md:mb-0 md:row-start-2 md:col-start-3  md:col-end-5 md:pl-5">
         Prénom
         <input
           class="border-solid border-[1px] border-[#0B3168] pl-2 md:h-12 rounded-md"
@@ -35,19 +35,19 @@ export const ContactForm = component$(() => {
           aria-label="entrez votre prénom"
         />
       </label>
-      <label class="md:col-span-1 md:row-start-3 md:col-start-1  md:col-end-3 md:flex md:flex-col md:pr-5">
+      <label class="row-start-4 col-start-1 col-end-5 md:col-end-3 flex flex-col col-span-4 md:col-span-2 md:row-start-3 md:pr-5">
         Mail
         <input
-          class="border-solid border-[1px] border-[#0B3168] pl-2 md:h-12 rounded-md"
+          class="border-solid border-[1px] border-[#0B3168] pl-2 mb-3 md:mb-0 md:h-12 rounded-md"
           required
           type="email"
           aria-label="entrez votre e-mail"
         />
       </label>
-      <label class="md:col-span-2 md:row-start-3 md:col-start-3  md:col-end-5 md:flex md:flex-col md:pl-5">
+      <label class="row-start-5 col-start-1 col-end-5 flex flex-col col-span-4 md:col-span-2 md:row-start-3 md:col-start-3 md:col-end-5 md:pl-5">
         Téléphone
         <input
-          class="border-solid border-[1px] border-[#0B3168] pl-2 md:h-12 rounded-md"
+          class="border-solid border-[1px] border-[#0B3168] pl-2 mb-6 md:mb-0 md:h-12 rounded-md"
           required
           type="tel"
           aria-label="entrez votre numéro de téléphone"
@@ -55,15 +55,15 @@ export const ContactForm = component$(() => {
       </label>
 
       <textarea
-        class="border-solid border-[1px] border-[#0B3168] md:col-span-4 pl-2 md:row-start-4 md:col-start-1  md:col-end-5"
+        class="border-solid border-[1px] border-[#0B3168] col-span-4 row-start-6 col-start-1 col-end-5 mb-6 md:mb-0 md:pl-2 md:row-start-4"
         placeholder="Votre texte"
         required
         aria-label="zone pour écrire les détails de votre demande"
       ></textarea>
-      <button class="h-14 mr-2 rounded-md hover:border-2 hover:border-[#0B3168] md:col-span-1 md:row-start-5 md:col-start-3  md:col-end-4 md:mt-14">
+      <button class="h-14 mr-2 rounded-md hover:border-2 hover:border-[#0B3168] row-start-7 col-start-1 col-end-2 col-span-1 md:row-start-5 md:col-start-3  md:col-end-4 md:mt-14">
         Effacer
       </button>
-      <button class="bg-[#0B3168] h-14 rounded-md text-white md:col-span-1 md:row-start-5 md:col-start-4  md:col-end-4 md:mt-14">
+      <button class="bg-[#0B3168] h-14 rounded-md text-white row-start-7 col-start-2 col-end-3 col-span-1 md:row-start-5 md:col-start-4  md:col-end-4 md:mt-14">
         Envoyer
       </button>
     </form>
