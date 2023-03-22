dd54db39975f65d12e2472129b4c14048b76a9a7
matthieu <georgematthieu1@gmail.com>
2023-03-22 14:21:58 +0100

    QCM section responsive

--- src/components/cards/ServicesCard.tsx
+++ src/components/cards/ServicesCard.tsx
@@ -13,12 +13,12 @@ export const ServicesCard = component$((props: CardProps) => {
     <>
       <article aria-label="articles" class={`flex flex-col ${styles} pb-10 md:pb-0`}>
         <span
-          class={`flex flex-col justify-center items-center bg-no-repeat bg-[url(/images/bg-cards.png)] bg-[length:110rem_40rem] h-[18rem] w-[22.5rem]`}
+          class={`flex flex-col justify-center items-center bg-no-repeat bg-[url(/images/bg-cards.png)] bg-[length:110rem_30rem] md:bg-[length:110rem_40rem] h-[18rem] md:w-[22.5rem]`}
         >
           <img class="-mt-[5rem]" src={props.image} alt="" />
           <h2 class=" font-normal text-center text-2xl -mt-[5rem] text-white">{props.title}</h2>
         </span>
-        <span class={`flex flex-col items-top justify-start shadow-xl px-4 py-5 h-[30rem] w-[22.5rem]`}>
+        <span class={`flex flex-col items-top justify-start shadow-xl px-4 py-5 h-[30rem] md:w-[22.5rem]`}>
           <Slot name="services" />
           <h3 class="text-[#0B3168] font-semibold my-5 text-2xl">{props.label}</h3>
           <Slot name="content" />
