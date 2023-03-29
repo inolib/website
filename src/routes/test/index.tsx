0ec8d826170d2d59bd7838015b1948ab6696e285
Quentin Ferrari <ferrari.quentin@gmail.com>
2023-03-29 10:08:12 +0200

    modify : change test menu items to links

--- src/routes/test/index.tsx
+++ src/routes/test/index.tsx
@@ -1,6 +1,15 @@
 import { component$ } from "@builder.io/qwik";
 
-import { MenuButton, MenuItem, MenuItemList, Select, SelectButton, SelectOption, SelectOptionList } from "@inolib/ui";
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItemList,
+  Select,
+  SelectButton,
+  SelectOption,
+  SelectOptionList,
+} from "@inolib/ui";
 
 const people = [
   { id: 1, name: "Matthieu MEIGNAN", available: true },
@@ -8,10 +17,14 @@ const people = [
   { id: 3, name: "Matthieu GEORGE", available: true },
 ];
 
-const item = [
-  { id: 1, name: "Accessibilite", available: true },
-  { id: 2, name: "Témoignage", available: false },
-  { id: 3, name: "Contact", available: true },
+const page = [
+  { id: 1, type: "Actualité", available: true, value: "/actualites" },
+  { id: 2, type: "Accessibilite", available: true, value: "/accessibilite" },
+  { id: 3, type: "Présentation", available: true, value: "/presentation" },
+  { id: 4, type: "Services", available: true, value: "/services" },
+  { id: 5, type: "Témoignages", available: true, value: "/temoignages" },
+  { id: 6, type: "Contact", available: true, value: "/contact" },
+  { id: 7, type: "Connexion", available: true, value: "/profile" },
 ];
 
 export default component$(() => {
@@ -39,10 +52,15 @@ export default component$(() => {
         </SelectOptionList>
       </Select>
       <Menu>
-        <MenuButton>click me</MenuButton>
-        <MenuItem>
-          <MenuItemList></MenuItemList>
-        </MenuItem>
+        <MenuButton>click me (3)</MenuButton>
+
+        <MenuItemList>
+          {page.map((page) => (
+            <MenuItem disabled={!page.available} key={page.id} link={page.value}>
+              {page.type}
+            </MenuItem>
+          ))}
+        </MenuItemList>
       </Menu>
     </>
   );
