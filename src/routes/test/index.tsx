0e7be4096672b3b36150cf6fa32a1136973fb65b
matthieu <georgematthieu1@gmail.com>
2023-04-05 16:30:55 +0200

    tabs integration

--- src/routes/test/index.tsx
+++ src/routes/test/index.tsx
@@ -1,33 +1,30 @@
 import { component$ } from "@builder.io/qwik";
 
-import { Tabs, TabsListItem, TabsPanels, TabsPanel, TabsItem } from "@inolib/ui";
+import { Tabs, TabsTabs, TabsPanels, TabsPanel, TabsTab } from "@inolib/ui";
 
 const tabs = [
-  { id: 1, name: "Mes informations", selected: true, panelId: 1 },
-  { id: 2, name: "Mot de passe", selected: false, panelId: 2 },
-  { id: 3, name: "Mes documents", selected: false, panelId: 3 },
-  { id: 4, name: "QCM", selected: false, panelId: 4 },
+  { id: "1", name: "Mes informations", selected: true, controls: "1" },
+  { id: "2", name: "Mot de passe", selected: false, controls: "2" },
+  { id: "3", name: "Mes documents", selected: false, controls: "3" },
+  { id: "4", name: "QCM", selected: false, controls: "4" },
 ];
-
-console.log("tab name :", tabs.name);
-console.log("tab id :", tabs.id);
 
 export default component$(() => {
   return (
     <>
       <Tabs>
-        <TabsListItem>
+        <TabsTabs>
           {tabs.map((tabs) => (
-            <TabsItem panelId={tabs.panelId} selected={tabs.selected} key={tabs.id}>
+            <TabsTab controls={tabs.controls} selected={tabs.selected} key={tabs.id}>
               {tabs.name}
-            </TabsItem>
+            </TabsTab>
           ))}
-        </TabsListItem>
+        </TabsTabs>
         <TabsPanels>
-          <TabsPanel id={1}>GG1</TabsPanel>
-          <TabsPanel id={2}>GG2</TabsPanel>
-          <TabsPanel id={3}>GG3</TabsPanel>
-          <TabsPanel id={4}>GG4</TabsPanel>
+          <TabsPanel id="1">GG1</TabsPanel>
+          <TabsPanel id="2">GG2</TabsPanel>
+          <TabsPanel id="3">GG3</TabsPanel>
+          <TabsPanel id="4">GG4</TabsPanel>
         </TabsPanels>
       </Tabs>
     </>
