7e7bea8fce9c446e4a9f045b0db6a7d859b0d1e6
Matthieu MEIGNAN <matthieu.meignan@inolib.com>
2023-04-13 10:49:54 +0200

    chore: add Vercel Edge

---
+++ src/entry.vercel-edge.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+
+import { createQwikCity, type PlatformVercel } from "@builder.io/qwik-city/middleware/vercel-edge";
+import qwikCityPlan from "@qwik-city-plan";
+import { manifest } from "@qwik-client-manifest";
+import render from "./entry.ssr";
+
+declare global {
+  interface QwikCityPlatform extends PlatformVercel {}
+}
+
+export default createQwikCity({ render, qwikCityPlan, manifest });
