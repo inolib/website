da40300811700fcb038f367b4bdbf0aff3810eb5
Quentin Ferrari <ferrari.quentin@gmail.com>
2023-04-13 15:32:04 +0200

    modif page rejoindre

--- src/routes/rejoindre/index.tsx
+++ src/routes/rejoindre/index.tsx
@@ -6,25 +6,29 @@ export default component$(() => {
   return (
     <>
       <h1>Rejoingnez-nous !</h1>
-      <form class="grid-rows-10 mx-[3rem] grid grid-cols-2 py-14 md:w-2/3 md:grid-rows-9 md:px-10">
-        <Filter labels={{ button: "Civilité*", items: ["Madame", "Monsieur", "Autre"] }} />
+      <form class="grid-rows-10 mx-[3rem] grid grid-cols-2 py-14 md:w-2/3 md:grid-rows-9 md:px-10 border-[1px] border-solid border-[#0B3168]">
+        <label class="row-start-1 col-span-1 md:col-start-1 md:col-span-1" aria-label="Renseignez-votre civilité">
+          <Filter labels={{ button: "Civilité*", items: ["Madame", "Monsieur", "Autre"] }} />
+        </label>
         <label class="row-start-2 col-span-2 md:col-start-1 md:col-span-1">
           Nom* :
-          <input type="text" required />
+          <input type="text" required maxLength={50} aria-label="Renseignez votre nom" />
         </label>
         <label class="row-start-3 col-span-2 md:row-start-2 md:col-start-2 md:col-span-1">
           Prénom* :
-          <input type="text" required />
+          <input type="text" required maxLength={50} aria-label="Renseignez votre prénom" />
         </label>
         <label class="row-start-4 col-span-2 md:row-start-3 md:col-start-1">
           E-mail* :
-          <input type="email" required />
+          <input type="email" required maxLength={50} aria-label="renseignez votre E-mail" />
         </label>
         <label class="row-start-5 col-span-2 md:row-start-4 md:col-start-1 md:col-span-1">
           Téléphone* :
-          <input type="tel" required />
+          <input type="tel" required maxLength={20} aria-label="renseignez votre numero de téléphone" />
         </label>
-        <Filter labels={{ button: "Objet de votre demande", items: ["CDD", "CDI", "Alternance", "Stage"] }} />
+        <label>
+          <Filter labels={{ button: "Objet de votre demande", items: ["CDD", "CDI", "Alternance", "Stage"] }} />
+        </label>
         <label class="row-start-7 col-span-2 md:row-start-6 md:col-start-1">
           Message* :<textarea></textarea>
         </label>
