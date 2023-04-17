3ed8f80030cf551c98903109bf2b7abfe78017a0
Quentin Ferrari <ferrari.quentin@gmail.com>
2023-04-17 09:28:09 +0200

    commit pre-merge

--- src/routes/rejoindre/index.tsx
+++ src/routes/rejoindre/index.tsx
@@ -1,52 +1,107 @@
 import { component$ } from "@builder.io/qwik";
 import { type DocumentHead } from "@builder.io/qwik-city";
-import { Filter } from "~/components/ui/composite/Filter";
 
 export default component$(() => {
   return (
-    <>
-      <h1>Rejoingnez-nous !</h1>
-      <form class="grid-rows-10 mx-[3rem] grid grid-cols-2 py-14 md:w-2/3 md:grid-rows-9 md:px-10 border-[1px] border-solid border-[#0B3168]">
-        <label class="row-start-1 col-span-1 md:col-start-1 md:col-span-1" aria-label="Renseignez-votre civilité">
-          <Filter labels={{ button: "Civilité*", items: ["Madame", "Monsieur", "Autre"] }} />
-        </label>
-        <label class="row-start-2 col-span-2 md:col-start-1 md:col-span-1">
+    <div class="flex flex-col items-center text-[#0B3168] my-14">
+      <h1 class="mb-10 max-w-sm text-3xl font-semibold md:mb-5 md:text-3xl">Rejoignez-nous !</h1>
+      <form class="grid-rows-11 mx-[3rem] grid grid-cols-2 py-14 md:w-2/3 md:grid-rows-9 md:px-10 border-[1px] border-solid border-[#0B3168]">
+        <select
+          class="row-start-1 col-span-1 md:col-start-1 md:col-span-1 h-12 rounded-md border-[1px] border-solid border-[#0B3168] m-2"
+          aria-label="Renseignez-votre civilité"
+          name=""
+        >
+          <option value="" disabled selected hidden>
+            Choix de votre civilité*
+          </option>
+          <option value="Madame">Madame</option>
+          <option value="Monsieur">Monsieur</option>
+          <option value="Autres">Autres</option>
+        </select>
+
+        <label class="row-start-2 col-span-2 flex flex-col md:col-start-1 md:col-span-1 mx-2">
           Nom* :
-          <input type="text" required maxLength={50} aria-label="Renseignez votre nom" />
+          <input
+            class="h-12 rounded-md border-[1px] border-solid border-[#0B3168] my-2"
+            type="text"
+            required
+            maxLength={50}
+            aria-label="Renseignez votre nom"
+          />
         </label>
-        <label class="row-start-3 col-span-2 md:row-start-2 md:col-start-2 md:col-span-1">
+        <label class="row-start-3 col-span-2 flex flex-col md:row-start-2 md:col-start-2 md:col-span-1 mx-2">
           Prénom* :
-          <input type="text" required maxLength={50} aria-label="Renseignez votre prénom" />
+          <input
+            class="h-12 rounded-md border-[1px] border-solid border-[#0B3168] my-2"
+            type="text"
+            required
+            maxLength={50}
+            aria-label="Renseignez votre prénom"
+          />
         </label>
-        <label class="row-start-4 col-span-2 md:row-start-3 md:col-start-1">
+        <label class="row-start-4 col-span-2 flex flex-col md:row-start-3 md:col-start-1 md:col-span-1 mx-2">
           E-mail* :
-          <input type="email" required maxLength={50} aria-label="renseignez votre E-mail" />
+          <input
+            class="h-12 rounded-md border-[1px] border-solid border-[#0B3168] my-2"
+            type="email"
+            required
+            maxLength={50}
+            aria-label="renseignez votre E-mail"
+          />
         </label>
-        <label class="row-start-5 col-span-2 md:row-start-4 md:col-start-1 md:col-span-1">
+        <label class="row-start-5 col-span-2 flex flex-col md:row-start-3 md:col-start-2 md:col-span-1 mx-2">
           Téléphone* :
-          <input type="tel" required maxLength={20} aria-label="renseignez votre numero de téléphone" />
+          <input
+            class="h-12 rounded-md border-[1px] border-solid border-[#0B3168] my-2"
+            type="tel"
+            required
+            maxLength={20}
+            aria-label="renseignez votre numero de téléphone"
+          />
         </label>
-        <label>
-          <Filter labels={{ button: "Objet de votre demande", items: ["CDD", "CDI", "Alternance", "Stage"] }} />
-        </label>
-        <label class="row-start-7 col-span-2 md:row-start-6 md:col-start-1">
-          Message* :<textarea></textarea>
-        </label>
-        <label class="row-start-8 col-span-2 md:row-start-7 md:col-start-1">
+
+        <select class="h-12 rounded-md border-[1px] border-solid border-[#0B3168] m-2">
+          <option value="" disabled selected hidden>
+            Objet de votre demande
+          </option>
+          <option value="CDD">CDD</option>
+          <option value="CDI">CDI</option>
+          <option value="Stage">Stage</option>
+          <option value="ALternance">Alternance</option>
+        </select>
+
+        <textarea
+          class="row-start-7 col-start-1 col-span-2 md:row-start-4 h-12 rounded-md border-[1px] border-solid border-[#0B3168] m-2"
+          placeholder="Sujet de votre demande"
+          id="textarea"
+          maxLength={1500}
+          required
+          aria-label="zone pour écrire les détails de votre demande"
+        ></textarea>
+        <label class="row-start-8 col-span-2 flex flex-col md:row-start-5 md:col-start-1 md:col-span-1 mx-2">
           Lien Linkedin :
-          <input type="text" />
+          <input class="h-12 rounded-md border-[1px] border-solid border-[#0B3168] my-2" type="text" />
         </label>
-        <label class="row-start-9 col-span-2 md:row-start-8 md:col-start-1">
+        <label class="row-start-9 col-span-2 flex flex-col md:row-start-5 md:col-start-2 md:col-span-1 mx-2">
           Lien Github :
-          <input type="text" />
+          <input class="h-12 rounded-md border-[1px] border-solid border-[#0B3168] my-2" type="text" />
         </label>
-        <div class="flex row-start-10 col-span-2 md:row-start-9 md:col-start-1">
-          <input type="checkbox" />
-          <p class="ml-2">J'ai lu et accepte les conditions d'utilisations</p>
+        <div class="flex row-start-10 col-span-2 md:row-start-6 md:col-start-1">
+          <input class="m-2" type="checkbox" />
+          <p class="ml-2">J'ai lu et accepte les conditions d'utilisations*</p>
         </div>
+        <button
+          class="row-start-11 col-start-1 col-span-1 md:row-start-7 h-14 rounded-md hover:border-2 hover:border-[#0B3168] m-2"
+          type="reset"
+        >
+          Effacer
+        </button>
+        <button class="row-start-11 col-start-2 col-span-1 md:row-start-7 h-14 rounded-md bg-[#0B3168] text-white m-2">
+          Envoyer
+        </button>
       </form>
-      <p>* champs obligatoire</p>
-    </>
+      <p class="mb-7 mt-4">* champs obligatoire</p>
+    </div>
   );
 });
 
