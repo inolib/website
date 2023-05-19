b90e23fc9f5797c711c20753567317550faf7025
matthieu <georgematthieu1@gmail.com>
2023-05-19 16:28:58 +0200

    fix

--- src/components/articles/ArticleHomeDisplay.tsx
+++ src/components/articles/ArticleHomeDisplay.tsx
@@ -1,7 +1,5 @@
 import { component$ } from "@builder.io/qwik";
 
-import { ArticleHomePage } from "./ArticleHomePage";
-
 type ArticleProps = {
   category?: string;
   content?: string;
@@ -19,35 +17,11 @@ export const ArticlesHomeDisplay = component$((props: ArticleProps) => {
         <h2 class="mt-10 text-4xl font-medium text-[#0B3168]">{props.title}</h2>
       </span>
       <span class="md:grid-md:col-2 md:grid-row-2- md:mt-14 md:grid">
-        <ArticleHomePage
-          src="/images/home1.png"
-          styles="md:col-end-1 md:ml-[8vw] md:w-[40vw]"
-          label="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          category="Développement"
-          date="20.Juin.2023"
-          alt=""
-        >
-          <p q:slot="content" class="max-w-md text-[#0B3168]">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est id, facilis veritatis repudiandae nobis eius
-            magnam non libero a dolor recusandae obcaecati similique eligendi magni aliquid nisi illum nemo explicabo.
-          </p>
-        </ArticleHomePage>
-        <ArticleHomePage
-          src="/images/home2.png"
-          styles="md:col-end-2 md:mr-[6rem]"
-          label="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          category="Accessibilité"
-          date="10.Mars.2023"
-          alt=""
-        />
-        <ArticleHomePage
-          src="/images/home3.png"
-          styles="md:col-end-2 md:mr-[6rem] md:-mt-[5rem]"
-          label="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          category="Accessibilité"
-          date="03.02.2023"
-          alt=""
-        />
+        <p q:slot="content" class="max-w-md text-[#0B3168]">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est id, facilis veritatis repudiandae nobis eius
+          magnam non libero a dolor recusandae obcaecati similique eligendi magni aliquid nisi illum nemo explicabo.
+        </p>
+
         <a
           aria-label="lien vers notre actualité"
           class="mr-[8rem] -mt-[18rem] ml-[8rem] hidden justify-end border-t border-slate-300 pt-8 font-normal text-[#0B3168] md:flex"
--- src/components/cards/CardsActualiteGridDisplay.tsx
+++ src/components/cards/CardsActualiteGridDisplay.tsx
@@ -1,60 +1,15 @@
 import { component$ } from "@builder.io/qwik";
 
-import { ArticleHomePage } from "../articles/ArticleHomePage";
 import { BlancArticleService } from "./BlancArticleService";
 
 export const CardsActualiteGridDisplay = component$(() => {
   return (
     <ul class="mx-[3rem] md:mx-[8rem] md:grid md:grid-cols-3 md:grid-rows-2 md:gap-20">
-      <li>
-        <ArticleHomePage
-          alt="article 1"
-          category="Accessibilité"
-          date="10.Mars.2023"
-          imgStyles="h-[14rem]"
-          src="/images/home7.png"
-          styles="h-[10rem]"
-        />
-      </li>
-      <li>
-        <ArticleHomePage
-          alt="article 2"
-          category="Accessibilité"
-          date="10.Mars.2023"
-          imgStyles="h-[14rem]"
-          src="/images/home2.png"
-          styles="h-[10rem]"
-        />
-      </li>
-      <li>
-        <ArticleHomePage
-          alt="article 3"
-          category="Formation"
-          date="10.Mars.2023"
-          label="Lorem ipsum dolor."
-          imgStyles="h-[14rem]"
-          src="/images/home6.png"
-          styles="h-[10rem]"
-        />
-      </li>
-      <li>
-        <ArticleHomePage
-          alt="article 4"
-          category="Inolib"
-          date="10.Mars.2023"
-          src="/images/home5.png"
-          styles="h-[14rem]"
-        />
-      </li>
-      <li>
-        <ArticleHomePage
-          alt="article 5"
-          category="Accessibilité"
-          date="10.Mars.2023"
-          src="/images/home3.png"
-          styles="h-[14rem]"
-        />
-      </li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
       <li>
         <BlancArticleService />
       </li>
