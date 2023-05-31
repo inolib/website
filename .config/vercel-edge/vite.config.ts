8531ea99394fa50db50d96ea1d3d3b574bc2e6c4
Matthieu MEIGNAN <matthieu.meignan@inolib.com>
2023-05-31 17:40:46 +0200

    chore: update config

--- .config/vercel-edge/vite.config.ts
+++ .config/vercel-edge/vite.config.ts
@@ -6,9 +6,9 @@ export default extendConfig(baseConfig, () => {
   return {
     plugins: [vercelEdgeAdapter()],
     build: {
-      outDir: "../../.vercel/output/functions/_qwik-city.func",
+      outDir: "./.vercel/output/functions/_qwik-city.func",
       rollupOptions: {
-        input: ["../../src/entry.vercel-edge.tsx", "@qwik-city-plan"],
+        input: ["./src/entry.vercel-edge.tsx", "@qwik-city-plan"],
       },
       ssr: true,
     },
