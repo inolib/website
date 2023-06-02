ef2e00be18bbe3db11f7d0d0cc9d0bf284841f77
Quentin Ferrari <ferrari.quentin@gmail.com>
2023-06-02 16:20:47 +0200

    ifx: contact page banner

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -15,7 +15,7 @@ export default component$(() => {
           content="Nous sommes heureux de pouvoir communiquer avec vous et sommes à votre disposition pour répondre à toutes vos questions
          ou remarques concernant notre site web ou nos services. Vous pouvez nous contacter en remplissant le formulaire de contact ci-dessous."
           imageStyle="md:w-3/5 md:max-w-2xl"
-          imageSrc="\images\contact-banner.png"
+          imageSrc="\images\contact-banner.jpeg"
           title="Contacts"
         />
       </header>
