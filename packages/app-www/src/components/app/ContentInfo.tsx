7d46ffe7b1a011a1006ae6959d5f40aebd55e5cd
Matthieu Meignan <matthieu.meignan@inolib.com>
2024-10-29 08:50:20 +0000

    fix qualiopi

--- packages/app-www/src/components/app/ContentInfo.tsx
+++ packages/app-www/src/components/app/ContentInfo.tsx
@@ -127,7 +127,7 @@ export const ContentInfo = () => {
                 alt="Certificat Qualiopi"
                 className="size-full"
                 height={150}
-                src="/images/logos/qualiopi/qualiopi.jpg"
+                src="/images/logos/qualiopi/qualiopi.png"
                 width={300}
               />
             </Link>
