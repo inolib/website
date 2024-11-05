6f4bc8c13ca95fd063a642717d5b2756f57ddece
Matthieu Meignan <matthieu.meignan@inolib.com>
2024-11-05 15:38:15 +0000

    add CSS transitions

--- packages/app-www/src/components/form/CheckboxField.tsx
+++ packages/app-www/src/components/form/CheckboxField.tsx
@@ -21,7 +21,7 @@ export const CheckboxField = forwardRef<HTMLInputElement, CheckboxFieldProps>(
             aria-describedby={`${id}-error`}
             aria-invalid={_error.value !== ""}
             className={cn(
-              "peer/input size-6 shrink-0 cursor-pointer appearance-none rounded-md border border-blue-600 outline-none checked:bg-blue-900 hover:bg-blue-50 hover:checked:bg-blue-900 focus-visible:outline-4 focus-visible:outline-offset-4 focus-visible:outline-blue-600",
+              "peer/input size-6 shrink-0 cursor-pointer appearance-none rounded-md border border-blue-600 outline-none transition-all duration-500 checked:bg-blue-900 hover:bg-blue-50 hover:checked:bg-blue-900 focus-visible:outline-4 focus-visible:outline-offset-4 focus-visible:outline-blue-600",
               {
                 "border-red-600 focus-visible:outline-red-600": _error.value !== "",
               },
@@ -33,13 +33,13 @@ export const CheckboxField = forwardRef<HTMLInputElement, CheckboxFieldProps>(
             {...passthru}
           />
 
+          <div className="pointer-events-none absolute flex size-6 items-center justify-center opacity-0 transition-all duration-500 peer-checked/input:opacity-100">
+            <CheckIcon className="stroke-white" />
+          </div>
+
           <label className="cursor-pointer" htmlFor={id}>
             {_label}
           </label>
-
-          <div className="pointer-events-none absolute hidden size-6 items-center justify-center peer-checked/input:flex">
-            <CheckIcon className="stroke-white" />
-          </div>
         </div>
 
         <p className="text-red-600" id={`${id}-error`}>
