0f94131cdf0ae24d4fddd80058909b8660f33c80
Matthieu Meignan <matthieu.meignan@inolib.com>
2024-11-06 11:12:42 +0000

    cleanup

--- packages/app-www/src/components/app/Contact.tsx
+++ packages/app-www/src/components/app/Contact.tsx
@@ -5,7 +5,7 @@ import { Section } from "~/components/section";
 export const Contact = () => {
   return (
     <Section className="flex flex-col items-center gap-24 bg-sand-50 bg-[url('/images/illustrations/app/contact.svg')] bg-[length:32.125rem_18.75rem] bg-[center_bottom_-7rem] bg-no-repeat pb-48 sm:bg-[length:40.15625rem_23.4375rem] sm:bg-[center_bottom_-8.7rem] md:bg-[length:48.1875rem_28.125rem] md:bg-[center_bottom_-10.4rem] lg:bg-[length:72.28125rem_42.1875rem] lg:bg-[center_bottom_-15.6rem]">
-      <Heading _alignment="center" _size="4xl" className="z-0">
+      <Heading _alignment="center" _size="4xl">
         <HeadingContent _level={2}>Une question, un devis, ou besoin d’échanger avec nous ?</HeadingContent>
         <HeadingSubheading>Contactez-nous dès maintenant pour en discuter.</HeadingSubheading>
       </Heading>
