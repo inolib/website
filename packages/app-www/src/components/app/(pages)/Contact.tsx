4907049c1a704b4fc6c696f4c841dc4a8b83bfd5
Matthieu Meignan <matthieu.meignan@inolib.com>
2024-11-09 01:13:38 +0000

    remove subheading

--- packages/app-www/src/components/app/(pages)/Contact.tsx
+++ packages/app-www/src/components/app/(pages)/Contact.tsx
@@ -1,14 +1,13 @@
-import { Heading, HeadingContent, HeadingSubheading } from "~/components/heading";
+import { Heading, HeadingContent } from "~/components/heading";
 import { Link } from "~/components/link";
 import { Section } from "~/components/section";
 
 export const Contact = () => {
   return (
     <Section>
-      <div className="flex flex-col items-center gap-8 overflow-hidden rounded-3xl bg-sand-50 p-8 sm:bg-[url('/images/illustrations/app/(pages)/contact.svg')] sm:bg-[length:37.5rem_9.375rem] sm:bg-[center_bottom_-3.2rem] sm:bg-no-repeat lg:bg-[length:56.25rem_14.0625rem] lg:bg-[center_bottom_-4.8rem] xl:bg-[length:75rem_18.75rem] xl:bg-[center_bottom_-6.5rem]">
+      <div className="flex flex-col items-center gap-16 overflow-hidden rounded-3xl bg-sand-50 p-8 sm:bg-[url('/images/illustrations/app/(pages)/contact.svg')] sm:bg-[length:37.5rem_9.375rem] sm:bg-[center_bottom_-3.2rem] sm:bg-no-repeat lg:bg-[length:56.25rem_14.0625rem] lg:bg-[center_bottom_-4.8rem] xl:bg-[length:75rem_18.75rem] xl:bg-[center_bottom_-6.5rem]">
         <Heading _alignment="center" _size="2xl">
-          <HeadingContent _level={2}>Une question, un devis, ou besoin d’échanger avec nous ?</HeadingContent>
-          <HeadingSubheading>Contactez-nous dès maintenant pour en discuter.</HeadingSubheading>
+          <HeadingContent _level={2}>Une question, un devis, ou envie d’échanger ?</HeadingContent>
         </Heading>
 
         <Link _color="blue-900" _shape="button" href="/contact">
--- packages/app-www/src/components/app/Contact.tsx
+++ packages/app-www/src/components/app/Contact.tsx
@@ -1,4 +1,4 @@
-import { Heading, HeadingContent, HeadingSubheading } from "~/components/heading";
+import { Heading, HeadingContent } from "~/components/heading";
 import { Link } from "~/components/link";
 import { Section } from "~/components/section";
 
@@ -6,8 +6,7 @@ export const Contact = () => {
   return (
     <Section className="flex flex-col items-center gap-24 bg-sand-50 bg-[url('/images/illustrations/app/contact.svg')] bg-[length:32.125rem_18.75rem] bg-[center_bottom_-7rem] bg-no-repeat pb-48 sm:bg-[length:40.15625rem_23.4375rem] sm:bg-[center_bottom_-8.7rem] md:bg-[length:48.1875rem_28.125rem] md:bg-[center_bottom_-10.4rem] lg:bg-[length:72.28125rem_42.1875rem] lg:bg-[center_bottom_-15.6rem]">
       <Heading _alignment="center" _size="4xl">
-        <HeadingContent _level={2}>Une question, un devis, ou besoin d’échanger avec nous ?</HeadingContent>
-        <HeadingSubheading>Contactez-nous dès maintenant pour en discuter.</HeadingSubheading>
+        <HeadingContent _level={2}>Une question, un devis, ou envie d’échanger ?</HeadingContent>
       </Heading>
 
       <Link _color="blue-900" _shape="button" href="/contact">
