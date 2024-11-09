f2b2d2ba03efa608cf33005aab10f7b503ba4b20
Matthieu Meignan <matthieu.meignan@inolib.com>
2024-11-09 16:30:17 +0000

    add CSS transitions

--- packages/app-www/src/components/faq/FAQGroup.tsx
+++ packages/app-www/src/components/faq/FAQGroup.tsx
@@ -4,5 +4,13 @@ import { cn } from "~/helpers";
 export type FAQGroupProps = BaseFAQGroupProps;
 
 export const FAQGroup = ({ className, ...passthru }: FAQGroupProps) => {
-  return <BaseFAQGroup className={cn("rounded-3xl [&[data-expanded=true]]:bg-blue-50", className)} {...passthru} />;
+  return (
+    <BaseFAQGroup
+      className={cn(
+        "rounded-3xl transition-all duration-200 ease-linear [&[data-expanded=true]]:bg-blue-50",
+        className,
+      )}
+      {...passthru}
+    />
+  );
 };
