672011acee714e130d2c924dc4fe74be926d6aae
Matthieu Meignan <matthieu.meignan@inolib.com>
2024-11-09 13:35:39 +0000

    fix design

--- packages/app-www/src/components/form/CheckboxField.tsx
+++ packages/app-www/src/components/form/CheckboxField.tsx
@@ -21,7 +21,7 @@ export const CheckboxField = forwardRef<HTMLInputElement, CheckboxFieldProps>(
             aria-describedby={`${id}-error`}
             aria-invalid={_error.value !== ""}
             className={cn(
-              "peer/input size-6 shrink-0 cursor-pointer appearance-none rounded-md border border-blue-600 outline-none transition-all duration-200 ease-linear checked:bg-blue-900 hover:bg-blue-50 hover:checked:bg-blue-900 focus-visible:outline-4 focus-visible:outline-offset-4 focus-visible:outline-blue-600",
+              "size-6 shrink-0 cursor-pointer appearance-none rounded-md border border-blue-600 outline-none transition-all duration-200 ease-linear checked:bg-blue-900 hover:bg-blue-50 hover:checked:bg-blue-900 focus-visible:outline-4 focus-visible:outline-offset-4 focus-visible:outline-blue-600",
               {
                 "border-red-600 focus-visible:outline-red-600": _error.value !== "",
               },
@@ -33,8 +33,8 @@ export const CheckboxField = forwardRef<HTMLInputElement, CheckboxFieldProps>(
             {...passthru}
           />
 
-          <div className="pointer-events-none absolute flex size-6 items-center justify-center opacity-0 transition-all duration-200 ease-linear peer-checked/input:opacity-100">
-            <CheckIcon className="stroke-white" />
+          <div className="pointer-events-none absolute flex size-6 items-center justify-center">
+            <CheckIcon className="size-0 stroke-white transition-all duration-200 ease-linear [:checked+div>&]:size-[1.125rem]" />
           </div>
 
           <label className="cursor-pointer" htmlFor={id}>
