3e59a3b067d10597b5d8a9d32260684fe19a71f9
Matthieu Meignan <matthieu.meignan@inolib.com>
2024-11-14 17:55:06 +0000

    cleanup

--- packages/config-eslint/dist/index.js
+++ packages/config-eslint/dist/index.js
@@ -77,7 +77,7 @@ export default typescriptESLint.config(
       reportUnusedDisableDirectives: "error",
     },
     rules: {
-      // ESLint
+      // eslint
       "no-irregular-whitespace": "off",
 
       // import-x
