9a7e94a8e358994ac74ccf4ff9502d08b45216ab
IssaDia <44264590+IssaDia@users.noreply.github.com>
2025-01-21 16:19:19 +0100

    set config strapi after cloning

--- packages/app-strapi/config/admin.ts
+++ packages/app-strapi/config/admin.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', process.env.ADMIN_JWT_SECRET),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
