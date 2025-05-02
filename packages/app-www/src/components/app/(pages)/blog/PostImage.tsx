e0d91fe1a07d44695cbd49237cfceea9d4b3ea0b
IssaDia <44264590+IssaDia@users.noreply.github.com>
2025-05-02 08:55:25 +0200

    fix single formation image display

--- packages/app-www/src/components/app/(pages)/blog/PostImage.tsx
+++ packages/app-www/src/components/app/(pages)/blog/PostImage.tsx
@@ -8,5 +8,13 @@ type PostImageProps = {
 export const PostImage = ({ imageUrl, title }: PostImageProps) => {
   if (!imageUrl) return null;
 
-  return <Image alt={title} className="mt-6 w-full rounded-lg object-cover" height={450} src={imageUrl} width={800} />;
+  return (
+    <Image
+      alt={title}
+      className="mt-6 w-full rounded-lg object-cover"
+      height={450}
+      src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${imageUrl}`}
+      width={800}
+    />
+  );
 };
